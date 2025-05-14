9f9240c1761452c5a31a0dcb60adbffc05c02302
denid88 <denisdubov88@gmail.com>
2025-05-14 17:27:38 +0300

    Init project

---
+++ FancyTodo/Resources/AppColors.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class AppColors {
+    static let background = UIColor(named: "background")
+    static let primaryText = UIColor(named: "primaryText")
+    static let statusText = UIColor(named: "statusText")
+}
---
+++ FancyTodo/Resources/AppFonts.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class AppFonts {
+    static let title = UIFont(name: "Inter28pt-Bold", size: 32.0) ?? UIFont.systemFont(ofSize: 32.0)
+    static let status = UIFont(name: "Inter28pt-SemiBold", size: 14.0) ?? UIFont.systemFont(ofSize: 14.0)
+}
